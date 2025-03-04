c782da7498cd3c6475e0c66a390c20f2b7f7c91e
Pierre Slamich <pierre@openfoodfacts.org>
2025-03-04 18:00:06 +0100

    Add project_type parameter to generate_file_path function

--- knowledge_panels/build_html.py
+++ knowledge_panels/build_html.py
@@ -30,6 +30,7 @@ class KnowledgeContentItem(BaseModel):
 def generate_file_path(
     root_dir: Path,
     item: KnowledgeContentItem,
+    project_type: str,
     tag_type: str,
     country: str,
     lang: str,
